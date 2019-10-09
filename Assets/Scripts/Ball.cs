1d72abe0b5eaebd35d54c6957e906c91dcf59667
cly4auHo <arsen.dmitriev96@gmail.com>
2019-10-09 23:23:54 +0300

    first

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private int scale = 200;
+    [SerializeField] private float screenSize = 10;
+
+    [SerializeField] private float speed = 15f;
+
+    private float clicked = 0;
+    private float clicktime = 0;
+    private float clickdelay = 0.5f;
+
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    bool IsDouble()
+    {
+        return false;
+    }
+    
+}
